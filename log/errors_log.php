e9610ec4695cd65419315904319c896b832941bb
lecomte38 <72401953+lecomte38@users.noreply.github.com>
2022-10-27 20:28:26 +0200

    :bug: error log

--- log/errors_log.php
+++ log/errors_log.php
@@ -1,5 +1,5 @@
 <?php
 	ini_set('display_errors', 'Off');
 	ini_set('log_errors', 'On');
-	ini_set('error_log', dirname(__file__) . '/error.log');
+	ini_set('error_log', dirname(__file__) . '/error.log'); 
 ?>
